9a4759dd074ced8c0c9cc9e24e57312526d152fe
Angga Pamungkas <kap21kap@gmail.com>
2022-03-28 16:57:30 +0800

    save

--- database/mysql_db.go
+++ database/mysql_db.go
@@ -11,7 +11,7 @@ import (
 )
 
 func InitMysqlDB() (*gorm.DB, error) {
-	dsn := "root:@tcp(127.0.0.1:3306)/todo?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:password@tcp(127.0.0.1:3306)/todo?charset=utf8mb4&parseTime=True&loc=Local"
 	result, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("DB CONNECTION ERROR")
